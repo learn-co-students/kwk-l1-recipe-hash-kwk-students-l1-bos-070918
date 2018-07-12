dfa59cbda63e4fcb1182e369e9c7db7bbbfac010
Kelly Cotter <kjc853@gmail.com>
2018-07-12 15:00:56 +0000

    Done.

---
+++ friends.rb
@@ -0,0 +1,25 @@
+character_list = {
+  "Monica" => "Neat Freak",
+  "Joey" => "Jokester",
+  "Chandler" => "Sarcastic",
+  "Pheobe" => "Free spirit",
+  "Rachel" => "Shopaholic",
+  "Ross" => "Divorcee",
+  
+}
+
+character_list.each do |character_name, character_adjective|
+  puts "Character name #{character_name}"
+  puts "Character adjective #{character_adjective}"
+  puts ""
+end
+
+
+
+
+
+
+
+
+
+
--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,5 +1,20 @@
 #code your solutions here
 
+perfect_10_recipe = {
+  :almond_flour => "2 cups", 
+  :gluten_freewhole_oats => "1 cup",
+  :kosher_salt => ".5 teaspoon",
+  :baking_powder => ".5 teaspoon",
+  :baking_soda => ".25 teaspoon",
+  :xanthan_gum => ".5 teaspoon",
+  :sliverd_almonds_lightly_toasted => ".25 cup",
+  :mini_dark_chocolate_chips => ".75 cup",
+  :olive_oil_ => ".5 cups",
+  :agve => ".25 cups",
+}
+
+
+
 #1. Create a hash called `perfect_10_recipe` that has the following data:
 #  - almond flour - 2 cups
 #  - gluten freewhole oats - 1 cup
@@ -14,12 +29,29 @@
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
-def recipe_ingredients(perfect_10_recipe)
-end
+
+
+  perfect_10_recipe.each do |ingredients, amount|
+    puts "Ingredient: #{ingredients}"
+    puts "Amount: #{amount}"
+  end
+  
+  
+  
+
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
-def amount_of_chocolate_chips(perfect_10_recipe)
+
+def amount_of_chocolate_chips(perfect_10_recipe = :mini_dark_chocolate_chips)
+  perfect_10_recipe.each do |ingredient, amount|
+    puts "ingredients: #{ingredient[7]}"
+    puts "amount: #{amount[7]}"
 end
+end
+
+amount_of_chocolate_chips(perfect_10_recipe)
+
+
 
 #3: Use `puts` to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the `each` method.
 def ingredients_and_amounts(perfect_10_recipe)
